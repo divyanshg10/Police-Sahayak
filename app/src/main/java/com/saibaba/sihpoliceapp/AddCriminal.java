79c039eeb4454f06adc3c42004779ac09aa36bf0
DELL <payalkhattri2@gmail.com>
2020-01-16 16:35:41 +0530

    add criminal

---
+++ app/src/main/java/com/saibaba/sihpoliceapp/AddCriminal.java
@@ -0,0 +1,31 @@
+package com.saibaba.sihpoliceapp;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AddCriminal extends Fragment {
+
+
+    public AddCriminal() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_criminal, container, false);
+    }
+
+}
